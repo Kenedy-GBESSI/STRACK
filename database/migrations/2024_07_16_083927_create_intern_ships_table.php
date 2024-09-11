288a0e12c6e3a68a7dd7309e73f006b3466570c3
GBESSI Kenedy <gbessikenedy@gmail.com>
2024-09-11 12:02:35 +0100

    UPDATE

--- database/migrations/2024_07_16_083927_create_intern_ships_table.php
+++ database/migrations/2024_07_16_083927_create_intern_ships_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('intern_ships', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description')->nullable();
+            $table->text('description')->nullable();
             $table->date('start_date');
             $table->date('end_date');
             $table->string('academic_year');
